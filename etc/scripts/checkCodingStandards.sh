00417534fbf1e6ae8943bd6c3f01773909c0c88c
Michael Zapf <mz@solutiondrive.de>
2018-08-02 15:03:49 +0200

    adjust checkCodingStandards script

--- etc/scripts/checkCodingStandards.sh
+++ etc/scripts/checkCodingStandards.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-FILTERED_FOLDERS=`find ./ -mindepth 1 -maxdepth 1 -type d | grep -Ev 'Resources|tests|etc|spec|logs|.phpspec|.git|vendor|.idea'`
-SEPERATOR=" "
-FOLDERS=$(printf "${SEPERATOR}%s" "${FILTERED_FOLDERS[@]}")
-
+FOLDERS="./src"
 vendor/bin/ecs-standalone.phar check --no-progress-bar -n -c vendor/bin/easy-coding-standard-php5.6.yml $FOLDERS $@
