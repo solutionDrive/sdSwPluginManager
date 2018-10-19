7a08b35a9749923b386d34734856cc478b1bfd90
Tobias Lückel <itsmegger@gmail.com>
2018-10-19 16:34:39 +0200

    Added an uninstall command based on the install command

---
+++ spec/Command/UninstallCommandSpec.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Created by solutionDrive GmbH
+ *
+ * @copyright 2018 solutionDrive GmbH
+ */
+
+namespace spec\sd\SwPluginManager\Command;
+
+use PhpSpec\ObjectBehavior;
+use sd\SwPluginManager\Command\UninstallCommand;
+use Symfony\Component\Console\Command\Command;
+
+class UninstallCommandSpec extends ObjectBehavior
+{
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(UninstallCommand::class);
+        $this->shouldHaveType(Command::class);
+    }
+}
