fc8f91ca0f95c8bb60345c34dbd5e89a2d755e92
Julius Noack <julius.noack@netlogix.de>
2021-08-27 09:00:33 +0200

    Revert "Change to GitHub actions"

--- etc/scripts/checkCodingStandards.sh
+++ etc/scripts/checkCodingStandards.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 FOLDERS="./src ./spec"
-vendor/bin/ecs-standalone.phar check --no-progress-bar -n -c vendor/bin/easy-coding-standard.yml $FOLDERS $@
+vendor/bin/ecs-standalone.phar check --no-progress-bar -n -c vendor/bin/easy-coding-standard-php7.1.yml $FOLDERS $@
